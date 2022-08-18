c3e4ade8706abc8a8268bf5809c0f1e620668946
Keizo <keizokita1@gmail.com>
2022-08-18 15:36:04 -0300

    descrição breve do commit

---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: 'Keizo',
+    email: 'keizokita1@email.com'
+  }
+
+  onSubmit(form: any){
+    console.log(form);
+
+    console.log(this.usuario);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
